fa50c65df14210966e1052a19050b9b973bb1d5c
Andrea <andrea210701@gmail.com>
2018-07-21 15:28:19 +0000

    cleanup script

--- script.sh
+++ script.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-apt-get update && apt-get install -y wget gnupg
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|apt-key add -
-echo "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-6.0 main" | tee -a /etc/apt/sources.list
 
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|apt-key add -
-# Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
-dpkg --add-architecture i386 && apt-get update && apt-get install -y git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng &&
+dpkg --add-architecture i386 
+apt-get update 
+apt-get install -y git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 /pipeline/build/root/toolchain/aarch64-linux-android-4.9
 git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 /pipeline/build/root/toolchain/aarch64-linux-android-clang
 KERNEL_DIR=$PWD
